81327ad4e68031cd1aac81fdb6a4eb6b7603f73d
andrew-armstrong80 <35451916+andrew-armstrong80@users.noreply.github.com>
2025-09-24 14:50:22 +0100

    shell script updated

--- src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
@@ -4,21 +4,23 @@ import java.io.File
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.edge.{EdgeDriver, EdgeDriverService, EdgeOptions}
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
-import org.openqa.selenium.firefox.{FirefoxDriver, FirefoxOptions, GeckoDriverService}
+import org.openqa.selenium.firefox.{FirefoxDriver, FirefoxOptions}
 
 object BrowserDriver {
 
+  // Lazy instantiation of the driver based on system property
   lazy val instance: WebDriver = {
     sys.props.getOrElse("browser", throw new IllegalArgumentException("'browser' system property must be set")) match {
 
       case "edge" =>
-        // Get binary and driver paths from system properties
         val edgeBinary = sys.props.get("edge.binary")
         val driverPath = sys.props.getOrElse("webdriver.edge.driver",
           throw new IllegalArgumentException("System property 'webdriver.edge.driver' must be set"))
 
         val edgeOptions = new EdgeOptions()
-        edgeOptions.addArguments("--headless=new") // optional headless
+        if (sys.props.get("headless").exists(_.toBoolean)) {
+          edgeOptions.addArguments("--headless=new")
+        }
         edgeBinary.foreach(edgeOptions.setBinary)
 
         val service = new EdgeDriverService.Builder()
@@ -29,16 +31,23 @@ object BrowserDriver {
 
       case "chrome" =>
         val chromeOptions = new ChromeOptions()
-        chromeOptions.addArguments("--headless=new")
+        if (sys.props.get("headless").exists(_.toBoolean)) {
+          chromeOptions.addArguments("--headless=new")
+        }
         new ChromeDriver(chromeOptions)
 
       case "firefox" =>
         val firefoxOptions = new FirefoxOptions()
-        firefoxOptions.addArguments("--headless=new")
+        if (sys.props.get("headless").exists(_.toBoolean)) {
+          firefoxOptions.addArguments("--headless=new")
+        }
         new FirefoxDriver(firefoxOptions)
 
       case other =>
         throw new IllegalArgumentException(s"Unsupported browser: $other")
     }
   }
+
+  // Implicit driver so existing tests can just do `driver.findElement(...)`
+  implicit def driver: WebDriver = instance
 }
