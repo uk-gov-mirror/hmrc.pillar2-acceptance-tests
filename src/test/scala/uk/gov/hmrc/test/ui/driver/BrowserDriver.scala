8c394a6fda4d5a411f77e052d2af66aebaaa4650
andrew-armstrong80 <35451916+andrew-armstrong80@users.noreply.github.com>
2025-09-24 14:34:47 +0100

    shell script updated

--- src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
@@ -1,12 +1,44 @@
-package uk.gov.hmrc.test.ui.driver
+package uk.gov.hmrc.selenium.webdriver
 
-import com.typesafe.scalalogging.LazyLogging
+import java.io.File
 import org.openqa.selenium.WebDriver
-import uk.gov.hmrc.selenium.webdriver.Driver
+import org.openqa.selenium.edge.{EdgeDriver, EdgeDriverService, EdgeOptions}
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import org.openqa.selenium.firefox.{FirefoxDriver, FirefoxOptions, GeckoDriverService}
 
-trait BrowserDriver extends LazyLogging {
-  logger.info(
-    s"Instantiating Browser: ${sys.props.getOrElse("browser", "'browser' System property not set. This is required")}"
-  )
-  implicit def driver: WebDriver = Driver.instance
+object BrowserDriver {
+
+  lazy val instance: WebDriver = {
+    sys.props.getOrElse("browser", throw new IllegalArgumentException("'browser' system property must be set")) match {
+
+      case "edge" =>
+        // Get binary and driver paths from system properties
+        val edgeBinary = sys.props.get("edge.binary")
+        val driverPath = sys.props.getOrElse("webdriver.edge.driver",
+          throw new IllegalArgumentException("System property 'webdriver.edge.driver' must be set"))
+
+        val edgeOptions = new EdgeOptions()
+        edgeOptions.addArguments("--headless=new") // optional headless
+        edgeBinary.foreach(edgeOptions.setBinary)
+
+        val service = new EdgeDriverService.Builder()
+          .usingDriverExecutable(new File(driverPath))
+          .build()
+
+        new EdgeDriver(service, edgeOptions)
+
+      case "chrome" =>
+        val chromeOptions = new ChromeOptions()
+        chromeOptions.addArguments("--headless=new")
+        new ChromeDriver(chromeOptions)
+
+      case "firefox" =>
+        val firefoxOptions = new FirefoxOptions()
+        firefoxOptions.addArguments("--headless=new")
+        new FirefoxDriver(firefoxOptions)
+
+      case other =>
+        throw new IllegalArgumentException(s"Unsupported browser: $other")
+    }
+  }
 }
