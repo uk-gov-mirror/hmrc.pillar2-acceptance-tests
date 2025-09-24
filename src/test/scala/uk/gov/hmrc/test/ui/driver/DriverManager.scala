c289ec343dde10d82a59e508e8e97b60f7827dfa
andrew-armstrong80 <35451916+andrew-armstrong80@users.noreply.github.com>
2025-09-24 15:28:05 +0100

    shell script updated

---
+++ src/test/scala/uk/gov/hmrc/test/ui/driver/DriverManager.scala
@@ -0,0 +1,49 @@
+package uk.gov.hmrc.test.ui.driver
+
+import java.io.File
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.edge.{EdgeDriver, EdgeDriverService, EdgeOptions}
+import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
+import org.openqa.selenium.firefox.{FirefoxDriver, FirefoxOptions}
+
+object DriverManager {
+
+  lazy val instance: WebDriver = {
+    sys.props.getOrElse("browser", throw new IllegalArgumentException("'browser' system property must be set")) match {
+
+      case "edge" =>
+        val edgeBinary = sys.props.get("edge.binary")
+        val driverPath = sys.props.getOrElse("webdriver.edge.driver",
+          throw new IllegalArgumentException("System property 'webdriver.edge.driver' must be set"))
+
+        val edgeOptions = new EdgeOptions()
+        if (sys.props.get("headless").exists(_.toBoolean)) {
+          edgeOptions.addArguments("--headless=new")
+        }
+        edgeBinary.foreach(edgeOptions.setBinary)
+
+        val service = new EdgeDriverService.Builder()
+          .usingDriverExecutable(new File(driverPath))
+          .build()
+
+        new EdgeDriver(service, edgeOptions)
+
+      case "chrome" =>
+        val chromeOptions = new ChromeOptions()
+        if (sys.props.get("headless").exists(_.toBoolean)) {
+          chromeOptions.addArguments("--headless=new")
+        }
+        new ChromeDriver(chromeOptions)
+
+      case "firefox" =>
+        val firefoxOptions = new FirefoxOptions()
+        if (sys.props.get("headless").exists(_.toBoolean)) {
+          firefoxOptions.addArguments("--headless=new")
+        }
+        new FirefoxDriver(firefoxOptions)
+
+      case other =>
+        throw new IllegalArgumentException(s"Unsupported browser: $other")
+    }
+  }
+}
